81e150b02f1a2dbba8ca6819e80227aa665f17c7
Yannick Baudraz <yannickbaudraz@gmail.com>
2019-05-03 00:38:13 +0200

    Login -> model

--- model/usersManager.php
+++ model/usersManager.php
@@ -1,12 +1,35 @@
 <?php
 /**
- * Author   : nicolas.glassey@cpnv.ch
- * Project  : Epreuve_151
- * Created  : 09.04.2019 - 13:48
+ * Author   : yannick.baudraz@cpnv.ch
+ * Project  : Projet web BD
+ * Created  : 03.05.2019
  *
- * Last update :    [01.12.2018 author]
- *                  [add $logName in function setFullPath]
- * Git source  :    [link]
+ * Last update :    05.03.2019 yannick.baudraz@cpnv.ch
+ *                    function isLoginCorrect() added
+ * Git source  :    https://github.com/Groupe-AMY/Projet_Web_DB/blob/master/model/usersManager.php
  */
 
-?>
+/**
+ * Verify the user's login
+ *
+ * @param string $userEmailAddress : user's mail to request
+ * @param string $userPsw          : user's password to verify with the password in the database
+ * @return bool : "true" only if the user and psw match the database. In all other cases will be "false".
+ */
+function isLoginCorrect($userEmailAddress, $userPsw)
+{
+  $result = false;
+
+  $loginQuery = "SELECT userHashPsw FROM users WHERE userEmailAddress = ' . $userEmailAddress .'";
+
+  require_once 'model/dbConnector.php';
+  $queryResult = executeQuerySelect($loginQuery);
+
+  // If the user exists
+  if (count($queryResult) == 1) {
+    $userHashPsw = $queryResult[0]['userHashPsw'];
+    $result = password_verify($userPsw, $userHashPsw);
+  }
+
+  return $result;
+}
