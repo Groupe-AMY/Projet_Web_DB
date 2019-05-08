ca2f2b6d2914f8ac5e9c7a4e243f69c60713fb8e
Yannick Baudraz <yannickbaudraz@gmail.com>
2019-05-08 16:22:59 +0200

    Model -> Fusion of the articles in the cart

--- model/cartManager.php
+++ model/cartManager.php
@@ -4,27 +4,39 @@
  * Project  : 151_2019_code
  * Created  : 04.04.2019 - 18:48
  *
- * Last update :    [01.12.2018 author]
- *                  [add $logName in function setFullPath]
+ * Last update :    08.05.2019 yannick.baudraz@cpnv.ch
+ *                      Fusion of the articles in the cart
  * Git source  :    [link]
  */
 
 /**
+ * Update the cart according to the leasing's days and the snow's code
+ *
  * @param $currentCartArray
  * @param $snowCodeToAdd
  * @param $qtyOfSnowsToAdd
  * @param $howManyLeasingDays
  * @return array
  */
-
-function updateCart($currentCartArray, $snowCodeToAdd, $qtyOfSnowsToAdd, $howManyLeasingDays){
+function updateCart($currentCartArray, $snowCodeToAdd, $qtyOfSnowsToAdd, $howManyLeasingDays)
+{
     $cartUpdated = array();
-    if($currentCartArray != null){
+    $flagCart = false;
+
+    if ($currentCartArray != null) {
+        foreach ($currentCartArray as $index => $snow) {
+            if ($snowCodeToAdd === $snow["code"] && $howManyLeasingDays === $snow["nbD"]) {
+                $currentCartArray[$index]['qty'] += $qtyOfSnowsToAdd;
+                $flagCart = true;
+            }
+        }
+
         $cartUpdated = $currentCartArray;
     }
-    $newSnowLeasing = array('code' => $snowCodeToAdd, 'dateD' => Date("d-m-y"), 'nbD' => $howManyLeasingDays, 'qty' => $qtyOfSnowsToAdd);
-
-    array_push($cartUpdated, $newSnowLeasing);
+    if (!$flagCart) {
+        $newSnowLeasing = array('code' => $snowCodeToAdd, 'dateD' => Date("d-m-y"), 'nbD' => $howManyLeasingDays, 'qty' => $qtyOfSnowsToAdd);
+        array_push($cartUpdated, $newSnowLeasing);
+    }
 
     return $cartUpdated;
 }
